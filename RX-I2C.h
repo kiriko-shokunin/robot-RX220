6ce1e0b9f3d542fe15c89c1ee4a115aa41ebf2f4
Hidetoshi Muramatsu <sr1326114@yahoo.co.jp>
2019-05-07 21:13:56 +0900

    Create RX-I2C.h

---
+++ RX-I2C.h
@@ -0,0 +1,20 @@
+#ifndef __RX_I2C
+#define __RX_I2C
+#include "iodefine.h"
+#include "machine.h"
+
+ char send_data[50];
+ char receve_data[50];
+ char receve_data_out[50];
+ int send_data_num;
+ int receve_num;
+ int stop_disen_flag;
+ int receve_flag,receve_fin_flag;
+extern void init_riic0(void);
+extern void int_iic0_eei(void);
+extern void int_iic0_txi(void);
+extern void int_iic0_rxi(void);
+extern void int_iic0_tei(void);
+extern int iic0_send(char*,int,int);
+extern char* iic0_gets();
+#endif
